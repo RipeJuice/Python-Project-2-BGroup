857bb2b058a365dd185764436f6128ebe77e55c5
geschantz <geschantz@thewildcats.net>
2025-11-14 12:00:34 -0500

    Added music.py and imported mixer module

---
+++ Code/music.py
@@ -0,0 +1,7 @@
+#The purpose of music.py is to play background music during the game.
+
+#Importing PyGame
+import pygame
+
+#Initializing the mixer module to load and play sounds
+pygame.mixer.init()
