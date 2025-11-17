bc85a060e5090d70e009a81345e271e435105d32
geschantz <geschantz@thewildcats.net>
2025-11-17 10:18:58 -0500

    Added music.py, imported mixer module, and added functions for playing music

--- Code/music.py
+++ Code/music.py
@@ -5,3 +5,11 @@ import pygame
 
 #Initializing the mixer module to load and play sounds
 pygame.mixer.init()
+
+#Function to load audio file
+def load_music(filename):
+    pygame.mixer.music.load(filename)
+
+#Function to loop the music indefinitely by passing in -1 as a parameter
+def loop_music():
+    pygame.mixer.music.play(-1)
