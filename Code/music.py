ccfa39e839505638bdca31060637777324a9258b
geschantz <geschantz@thewildcats.net>
2025-11-17 12:01:35 -0500

    Added background_music_1.mp3 and code in music.py

--- Code/music.py
+++ Code/music.py
@@ -7,9 +7,12 @@ import pygame
 pygame.mixer.init()
 
 #Function to load audio file
-def load_music(filename):
-    pygame.mixer.music.load(filename)
+def load_music(filepath):
+    pygame.mixer.music.load(filepath)
 
 #Function to loop the music indefinitely by passing in -1 as a parameter
 def loop_music():
     pygame.mixer.music.play(-1)
+
+load_music("/Users/geschantz/PycharmProjects/Python-Project-2-BGroup/Code/background_music_1.mp3")
+loop_music()
